3bd095c211cddf79390144ae14e5efccc8c3b791
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-03-23 13:29:09 +0100

    Color output/signing/default config...

--- fedora/buildrpms.sh
+++ fedora/buildrpms.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+function print_info
+{
+  echo -e "\e[0;32m$1\033[0m"
+}
+
+function print_error
+{
+  echo -e "\033[0;31m$1\033[0m"
+}
+
 
 # Load default config
 source default.cfg
@@ -8,10 +18,19 @@ then
   source local.cfg
 fi
 
+# Get next release version number and increment after
+if [ ! -f version.cfg ]
+then
+  echo "RELVER=1" > version.cfg
+fi
+source version.cfg
+let RELVER+=1
+echo "RELVER=$RELVER" > version.cfg
+
 # Clean logfiles
 if [ -f $OUTDIR/build.log ]
 then
-  echo ".. Cleaning log file"
+  print_info "Cleaning log file"
   rm $OUTDIR/build.log
 fi
 
@@ -25,42 +44,57 @@ patch=$(grep -e 'SET(CPACK_PACKAGE_VERSION_PATCH' ../CMakeLists.txt |
 
 version=$(echo $major.$minor.$patch)
 
-echo ".. Building version $version"
+print_info "Building version $version"
 
 # Current git branch name
 branch=$(git branch --no-color 2> /dev/null |
     sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
 
-echo ".. Creating source tarball"
+print_info "Creating source tarball"
 # Create source tarball
 cd ..
 git archive --format=tar --prefix=qgis-$version/ $BRANCH | bzip2 >fedora/sources/qgis-$version.tar.gz
-cd fedora
+cd -
 
-echo ".. Creating source package"
+print_info "Creating source package"
 # Build source package
 mock --buildsrpm --spec qgis.spec --sources ./sources --define "_relver $RELVER" --resultdir=$OUTDIR
 if [ $? -ne 0 ]
 then
-  echo ".. Creating source package failed"
+  print_error "Creating source package failed"
   exit 1
 fi
 
 srpm=$(grep -e 'Wrote: .*\.src\.rpm' $OUTDIR/build.log |
     sed 's_Wrote: /builddir/build/SRPMS/\(.*\)_\1_')
 
-echo ".. Source package created: $srpm"
+print_info "Source package created: $srpm"
 
+# Create packages for every ARCH defined in the config file
 for arch in "${ARCHS[@]}"
 do :
-  echo ".. Building packages for $arch"
+  print_info "Building packages for $arch"
+  if [ -f $OUTDIR/$arch/build.log ]
+  then
+    print_info "Cleaning log file"
+    rm $OUTDIR/$arch/build.log
+  fi
   mkdir $OUTDIR/$arch
   mock -r $arch --rebuild $OUTDIR/$srpm --define "_relver $RELVER" --resultdir=$OUTDIR/$arch
-  if [ $? -eq 0 ]
+  if [  $? -eq 0 ]
   then
-    echo ".. Signing packages for $arch"
-    rpm --resign $OUTDIR/$arch/*-$version-$RELVER.*.rpm
+    # Add to package list
+    packages="$packages $(ls $OUTDIR/$arch/*-$version-$RELVER.*.rpm)"
   else
+    print_error "Package creation for $arch failed. Abort"
     exit 1
   fi
 done
+
+if [ $NOSIGN = false ]
+then
+  print_info "Signing packages"
+  rpm --resign $packages
+fi
+
+print_info "Done"

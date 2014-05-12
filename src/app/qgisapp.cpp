83e26cbef9c1b458db75f5ae2610599329a26156
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-05-13 01:13:13 +0200

    Python evalToObject

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -6732,6 +6732,15 @@ class QgsPythonRunnerImpl : public QgsPythonRunner
       return false;
     }
 
+    virtual void* evalToObject(const QString &command, const QString &siptype)
+    {
+      if ( mPythonUtils && mPythonUtils->isEnabled() )
+      {
+        return mPythonUtils->evalToObject( command, siptype );
+      }
+      return 0;
+    }
+
   protected:
     QgsPythonUtils* mPythonUtils;
 };
